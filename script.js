86533c3fd540796715b6c5e74a7afe904df203ad
Stuti Sinha <stutisinha712@gmail.com>
2024-02-28 17:16:40 +0530

    fixes

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
-const $city = document.getElementById('cityname').value;
-const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${$city}`;
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '621ae92574mshdf0e867fb36a90dp187fccjsn8de9c6536fc8',
-		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
-	}
-};
-
 async function fetchData() {
+    const city = document.getElementById('cityname').value;
+    const url = `https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=${city}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '621ae92574mshdf0e867fb36a90dp187fccjsn8de9c6536fc8',
+            'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+        }
+    };
+
     try {
         const response = await fetch(url, options);
-        const data = await response.json(); // Parse response as JSON
+        const data = await response.json();
         const cityElement = document.querySelector('.city');
+        const temperatureElement = document.querySelector('.temp');
         const humidityElement = document.querySelector('.humidity');
         const windElement = document.querySelector('.wind');
-        
-        cityElement.textContent = data.city_name; // Update city name
-        humidityElement.textContent = `${data.humidity}%`; // Update humidity
-        windElement.textContent = `${data.wind_speed} kmph`; // Update wind speed
+
+        cityElement.textContent = city;
+        humidityElement.textContent = `${data.humidity}%`;
+        temperatureElement.textContent = `${data.temp}°C`;
+        windElement.textContent = `${data.wind_speed} kmph`;
     } catch (error) {
         console.error(error);
     }
 }
-
-fetchData();
-
